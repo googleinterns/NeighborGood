a23f47cbdb49f4beec794d52524354bacf287b94
denzilbilson-google <denzilbilson@google.com>
2020-07-06 18:06:48 +0000

    add responsive map to admin page

--- src/main/webapp/admin_dashboard_script.js
+++ src/main/webapp/admin_dashboard_script.js
@@ -1,7 +1,17 @@
+var map;
+var styledMapType;
+var map_key = keys.MAP_KEY;
+
+load(`https://maps.googleapis.com/maps/api/js?key=${map_key}`); // Add maps API to html
+
 google.charts.load('current', {'packages':['line']});
 google.charts.setOnLoadCallback(drawChart);
-
 window.addEventListener("resize", drawChart);
+
+window.addEventListener('load', drawMap);
+
+
+//window.onload(drawMap());
 
 function drawChart() {
 	var data = new google.visualization.DataTable();
@@ -27,4 +37,107 @@ function drawChart() {
 	]);
 	var chart = new google.charts.Line(document.getElementById('chart-div'));
 	chart.draw(data);
+}
+
+
+
+function drawMap() {
+	// Styles a map in night mode.
+	var map = new google.maps.Map(document.getElementById('map-div'), {
+		center: {lat: 40.674, lng: -73.945},
+		zoom: 12,
+		styles: [
+			{elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+			{elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+			{elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+			{
+				featureType: 'administrative.locality',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#d59563'}]
+			},
+			{
+				featureType: 'poi',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#d59563'}]
+			},
+			{
+				featureType: 'poi.park',
+				elementType: 'geometry',
+				stylers: [{color: '#263c3f'}]
+			},
+			{
+				featureType: 'poi.park',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#6b9a76'}]
+			},
+			{
+				featureType: 'road',
+				elementType: 'geometry',
+				stylers: [{color: '#38414e'}]
+			},
+			{
+				featureType: 'road',
+				elementType: 'geometry.stroke',
+				stylers: [{color: '#212a37'}]
+			},
+			{
+				featureType: 'road',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#9ca5b3'}]
+			},
+			{
+				featureType: 'road.highway',
+				elementType: 'geometry',
+				stylers: [{color: '#746855'}]
+			},
+			{
+				featureType: 'road.highway',
+				elementType: 'geometry.stroke',
+				stylers: [{color: '#1f2835'}]
+			},
+			{
+				featureType: 'road.highway',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#f3d19c'}]
+			},
+			{
+				featureType: 'transit',
+				elementType: 'geometry',
+				stylers: [{color: '#2f3948'}]
+			},
+			{
+				featureType: 'transit.station',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#d59563'}]
+			},
+			{
+				featureType: 'water',
+				elementType: 'geometry',
+				stylers: [{color: '#17263c'}]
+			},
+			{
+				featureType: 'water',
+				elementType: 'labels.text.fill',
+				stylers: [{color: '#515c6d'}]
+			},
+			{
+				featureType: 'water',
+				elementType: 'labels.text.stroke',
+				stylers: [{color: '#17263c'}]
+			}
+		]
+	});
+	//Resize Function
+	google.maps.event.addDomListener(window, "resize", function() {
+		var center = map.getCenter();
+		google.maps.event.trigger(map, "resize");
+		map.setCenter(center);
+	});
+}
+
+function load(file){
+	var src = document.createElement("script");
+	src.setAttribute("type", "text/javascript");
+	src.setAttribute("src", file);
+	document.getElementsByTagName("head")[0].appendChild(src);
 }
