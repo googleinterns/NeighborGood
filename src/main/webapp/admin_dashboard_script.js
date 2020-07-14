6c950b2a64623e9e5e750d1eaf554c141285df5e
denzilbilson-google <denzilbilson@google.com>
2020-07-14 18:42:39 +0000

    add geolocation to map on admin page

--- src/main/webapp/admin_dashboard_script.js
+++ src/main/webapp/admin_dashboard_script.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-var map;
+var map, infoWindow;
 var styledMapType;
 var mapKey = config.MAPS_KEY;
 var userTasksArray;
@@ -149,6 +149,33 @@ function drawMap() {
 		google.maps.event.trigger(map, "resize");
 		map.setCenter(center);
 	});
+    infoWindow = new google.maps.InfoWindow;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('This is your Neighborhood!');
+			infoWindow.open(map);
+			map.setCenter(pos);
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
 }
 
 function load(file) {
