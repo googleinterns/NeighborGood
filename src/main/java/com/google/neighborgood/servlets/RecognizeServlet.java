323e5212200e3b219b18bd318b12666b97b9396a
Leonard Zhang <leonardzhang@google.com>
2020-07-16 22:38:28 +0000

    Complete the setup of RecognizeServlet.java file

--- src/main/java/com/google/neighborgood/servlets/RecognizeServlet.java
+++ src/main/java/com/google/neighborgood/servlets/RecognizeServlet.java
@@ -14,24 +14,37 @@
 
 package com.google.neighborgood.servlets;
 
+import com.google.api.gax.rpc.ClientStream;
 import com.google.api.gax.rpc.ResponseObserver;
 import com.google.api.gax.rpc.StreamController;
+import com.google.cloud.speech.v1.RecognitionConfig;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.StreamingRecognitionConfig;
 import com.google.cloud.speech.v1.StreamingRecognitionResult;
+import com.google.cloud.speech.v1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1.StreamingRecognizeResponse;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 
 /** Servlet that turns speech into text. */
 @WebServlet("/speech")
 public class RecognizeServlet extends HttpServlet {
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     SpeechClient client = SpeechClient.create();
     ResponseObserver<StreamingRecognizeResponse> responseObserver =
         new ResponseObserver<StreamingRecognizeResponse>() {
@@ -58,7 +71,86 @@ public class RecognizeServlet extends HttpServlet {
               SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
               finalResult += alternative.getTranscript();
             }
+            try {
+              response.setContentType("text/html;");
+              response.getWriter().println(finalResult);
+            } catch (IOException e) {
+              System.out.println("When printing response, exception " + e + " occurs.");
+            }
+          }
+
+          // Implement the onError() function called when receiving a terminating error
+          public void onError(Throwable t) {
+            System.err.println(t);
           }
         };
+
+    ClientStream<StreamingRecognizeRequest> clientStream =
+        client.streamingRecognizeCallable().splitCall(responseObserver);
+
+    // Define a recognition configuration with language set to English and sample rate set to
+    // 16000Hz
+    RecognitionConfig recognitionConfig =
+        RecognitionConfig.newBuilder()
+            .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+            .setLanguageCode("en-US")
+            .setSampleRateHertz(16000)
+            .build();
+
+    StreamingRecognitionConfig streamingRecognitionConfig =
+        StreamingRecognitionConfig.newBuilder().setConfig(recognitionConfig).build();
+
+    // The config will be the first reques
+    StreamingRecognizeRequest streamingRecognizeRequest =
+        StreamingRecognizeRequest.newBuilder()
+            .setStreamingConfig(streamingRecognitionConfig)
+            .build();
+
+    clientStream.send(streamingRecognizeRequest);
+
+    // Set the sample rate hertz of the audio format to 16000, sample size to 16bits and only one
+    // channel
+    AudioFormat audioFormat = new AudioFormat(16000, 16, 1, true, false);
+
+    // Receive input from the device microphone stream
+    DataLine.Info targetInfo = new Info(TargetDataLine.class, audioFormat);
+
+    if (!AudioSystem.isLineSupported(targetInfo)) {
+      // If microphone not supported, send an error message to the user
+      System.err.println("Microphone not supported");
+      response.setContentType("text/html;");
+      response.getWriter().println("Microphone is not supported on the device");
+      System.exit(0);
+    }
+
+    try {
+      TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+      targetDataLine.open(audioFormat);
+      targetDataLine.start();
+      long startTime = System.currentTimeMillis();
+
+      // Start receiving input audio stream
+      AudioInputStream audio = new AudioInputStream(targetDataLine);
+      while (true) {
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        byte[] data = new byte[6400];
+        audio.read(data);
+        // After 30 seconds, stop receiving input
+        if (estimatedTime > 30000) {
+          targetDataLine.stop();
+          targetDataLine.close();
+          break;
+        }
+        streamingRecognizeRequest =
+            StreamingRecognizeRequest.newBuilder()
+                .setAudioContent(ByteString.copyFrom(data))
+                .build();
+        clientStream.send(streamingRecognizeRequest);
+      }
+    } catch (LineUnavailableException e) {
+      System.out.println("An exception " + e + " is thrown.");
+    }
+
+    responseObserver.onComplete();
   }
 }
