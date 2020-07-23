d52651d00853837593305e4e8a1053a1ce2b7693
Leonard Zhang <leonardzhang@google.com>
2020-07-23 18:27:02 +0000

    Test the normal situation of sending single or multiple POST request to the MessageServlet

--- src/test/java/com/google/neighborgood/MessageServletTest.java
+++ src/test/java/com/google/neighborgood/MessageServletTest.java
@@ -16,12 +16,14 @@ package com.google.neighborgood.servlets;
 
 import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -57,10 +59,18 @@ public final class MessageServletTest {
 
   private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
   private final PrintStream originalErr = System.err;
+  private UserService userService;
+  private DatastoreService ds;
+  private HttpServletRequest request;
+  private HttpServletResponse response;
 
   @Before
   public void setUp() {
     helper.setUp();
+    userService = UserServiceFactory.getUserService();
+    ds = DatastoreServiceFactory.getDatastoreService();
+    request = mock(HttpServletRequest.class);
+    response = mock(HttpServletResponse.class);
   }
 
   @After
@@ -71,12 +81,10 @@ public final class MessageServletTest {
   @Test
   public void testEnvironmentTest() {
     // Test the UserService feature
-    UserService userService = UserServiceFactory.getUserService();
     assertTrue(userService.isUserAdmin());
     assertTrue(userService.isUserLoggedIn());
 
     // Test the DataStore feature
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
     assertEquals(0, ds.prepare(new Query("dummy")).countEntities(withLimit(10)));
     ds.put(new Entity("dummy"));
     ds.put(new Entity("dummy"));
@@ -84,14 +92,42 @@ public final class MessageServletTest {
   }
 
   @Test
-  public void testInsertOneMessage() {
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    HttpServletResponse response = mock(HttpServletResponse.class);
-
+  public void testInsertOneMessage() throws IOException {
     // Check whether the datastore is empty before the test
-    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
     assertEquals(0, ds.prepare(new Query("Message")).countEntities(withLimit(10)));
 
     when(request.getParameter("task-id")).thenReturn("1234567890");
+    when(request.getParameter("msg")).thenReturn("Testing message");
+
+    new MessageServlet().doPost(request, response);
+
+    // After sending the POST request, there should be one entity in the datastore
+    assertEquals(1, ds.prepare(new Query("Message")).countEntities(withLimit(10)));
+    PreparedQuery results = ds.prepare(new Query("Message"));
+    Entity entity = results.asSingleEntity();
+
+    // The entity can't be null
+    assertNotNull(entity);
+
+    // Test the stored personal information
+    assertEquals("Testing message", (String) entity.getProperty("message"));
+    assertEquals("1234567890", (String) entity.getProperty("taskId"));
+    assertEquals("1234567890", (String) entity.getProperty("sender"));
+  }
+
+  @Test
+  public void testInsertMultipleMessage() throws IOException {
+    // Check whether the datastore is empty before the test
+    assertEquals(0, ds.prepare(new Query("Message")).countEntities(withLimit(10)));
+
+    for (int i = 1; i < 11; i++) {
+      String index = Integer.toString(i);
+      when(request.getParameter("task-id")).thenReturn("1234567890" + index);
+      when(request.getParameter("msg")).thenReturn("Testing message " + index);
+
+      new MessageServlet().doPost(request, response);
+
+      assertEquals(i, ds.prepare(new Query("Message")).countEntities(withLimit(10)));
+    }
   }
 }
